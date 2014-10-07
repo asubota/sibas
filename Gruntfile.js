ebaf5f399ebfbf65233746cb01f0f97ad99ec495
asubota <asubota@cogniance.com>
2014-10-07 12:40:00 +0300

    trying fix font path

--- Gruntfile.js
+++ Gruntfile.js
@@ -350,10 +350,10 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
         }, {
-          expand: true,
-          cwd: '.',
-          src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
-          dest: '<%= yeoman.dist %>'
+        expand: true,
+          cwd: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap',
+          src: '*',
+          dest: '<%= yeoman.dist %>/fonts'
         }]
       },
       styles: {
